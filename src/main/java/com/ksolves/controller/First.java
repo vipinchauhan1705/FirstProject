7060a902ac201102c9ac4297894c4f926e68f949
vipin chauhan <vipinchauhanji72@gmail.com>
2021-03-10 16:22:17 +0530

    Added list of detail into controller  of Spring boot project

--- src/main/java/com/ksolves/controller/First.java
+++ src/main/java/com/ksolves/controller/First.java
@@ -3,12 +3,22 @@ package com.ksolves.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class First {
-    @GetMapping("/")
-    public String show()
+    @GetMapping("/show")
+    public List<Home> show()
     {
-        return "hello! geeks";
+
+        return Arrays.asList(
+            new Home("shiva","python ","btech") ,
+                new Home("priya","java","btech")
+        );
+
     }
 
 
--- src/main/java/com/ksolves/controller/Home.java
+++ src/main/java/com/ksolves/controller/Home.java
@@ -1,4 +1,38 @@
 package com.ksolves.controller;
 
 public class Home {
+    String name;
+    String course;
+    String Qualification;
+    public Home() { }
+
+    public Home(String name, String course, String qualification) {
+        this.name = name;
+        this.course = course;
+        Qualification = qualification;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getQualification() {
+        return Qualification;
+    }
+
+    public void setQualification(String qualification) {
+        Qualification = qualification;
+    }
 }
